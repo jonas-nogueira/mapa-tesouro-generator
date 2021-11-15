3a1d3d9433899ffb4d696a47e19982fc678e20bd
Jonas Nogueira <jonasjrsantos@gmail.com>
2021-11-15 12:19:24 -0300

    versão inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+import qrcode from "qrcode-generator";
+import Pdf from "react-to-pdf";
 import './App.css';
 
+const options = {
+  orientation: 'portrait',
+  unit: 'mm',
+  format: [210, 297]
+};
+
 function App() {
+  const [qrcodeData, setQrcodeData] = useState("");
+  const [qrcodes, setQrcodes] = useState([]);
+  const ref = useRef();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form">
+        <div className="label">Digite os valores separado por vírgula</div>
+        <textarea
+          className="values"
+          onChange={(evt) => setQrcodeData(evt.target.value)}
+        ></textarea>
+        <button onClick={() => {
+          const qrcodeValues = qrcodeData.split(",")
+            .map(item => {
+              var qr = qrcode(0, "H");
+              qr.addData(encodeURI(item));
+              qr.make();
+              return {
+                image: qr.createImgTag(5, 16),
+                label: item
+              };
+            });
+          setQrcodes(qrcodeValues);
+        }}>Gerar</button>
+      </div>
+      <div className="resultContainer">
+        <Pdf targetRef={ref} filename="code-example.pdf" options={options}>
+          {({ toPdf }) => <button onClick={toPdf}>Exportar</button>}
+        </Pdf>
+        <div className="result" ref={ref}>
+          {
+            qrcodes.map(qrcodeInfo => {
+              return (
+                <div
+                  className="qrcode-container"
+                  key={qrcodeInfo.label}
+                >
+                  <div className="qrcode-label">
+                    {
+                      qrcodeInfo.label
+                    }
+                  </div>
+                  <div
+                    className="qrcode-image"
+                    dangerouslySetInnerHTML={{ __html: qrcodeInfo.image }}
+                  />
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
